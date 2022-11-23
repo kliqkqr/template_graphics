8e5be9ad5fa0b3cccfb7380272dc062eef34ccb8
kliqkqr <paul.prietzel@gmail.com>
2022-11-23 21:17:21 +0100

    Vect implementations / traits for other primitives

--- src/prim/d2/mod.rs
+++ src/prim/d2/mod.rs
@@ -1,3 +1,7 @@
+// ----------------------------------------------------------------------------------------------------------------------------------------
+//   IMPORTS 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
 use std::clone::{
     Clone
 };
@@ -14,48 +18,90 @@ use std::ops::{
     Neg
 };
 
+use std::option::{
+    Option
+};
+
 use crate::util::num::{
     Num
 };
 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+//   TYPES 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
 /// 2D vector
-pub struct Vect<A : Num> (
-    pub A,
-    pub A
-);
+pub struct Vect<A : Num>(pub A, pub A);
 
 /// 2D line defined by 2 points
-pub struct PLine<A : Num> (
-    pub Vect<A>,
-    pub Vect<A>
-);
+pub struct PLine<A : Num> (pub Vect<A>, pub Vect<A>);
 
-/// 2D line defined by 1 point and 1 vector
+/// 2D line defined by 1 point and 1 direction vector
 pub struct VLine<A : Num> {
-    pub pos : Vect<A>,  // position  point
-    pub dir : Vect<A>   // direction vector
+    pub pos : Vect<A>,
+    pub dir : Vect<A>
 }
 
-/// 2D Triangle defined by 3 points
-pub struct PTri<A : Num> (
-    pub Vect<A>,
-    pub Vect<A>,
-    pub Vect<A>
-);
+/// 2D line segment defined by 1 point and 1 direction vector
+pub struct VLSeg<A : Num> {
+    pub pos : Vect<A>,
+    pub dir : Vect<A>
+}
 
-/// 2D Rectangle defined by 2 points and a left orthogonal distance
-pub struct DPRect<A : Num> {
+/// 2D triangle defined by 3 points
+pub struct PTri<A : Num> (pub Vect<A>, pub Vect<A>, pub Vect<A>);
+
+/// 2D square defined by 2 points a b where c = b + rotate(90, b - a) and d = a + rotate(90, b - a)
+pub struct PSqr<A : Num> (pub Vect<A>, pub Vect<A>);
+
+/// 2D rectangle defined by 2 points a b and a left orthogonal distance ratio where c = b + disr * rotate(90, b - a) and d = a + disr * rotate(90, b - a)
+pub struct PDRect<A : Num> {
     pub pnts : (Vect<A>, Vect<A>),
-    pub dist : A  
+    pub disr : A  
 }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+//   IMPLEMENTATIONS 
+// ----------------------------------------------------------------------------------------------------------------------------------------
 
 impl<A : Num> Vect<A> {
-    pub fn new(x : A, y : A) -> Vect<A> {
-        Vect(x, y)
+    pub fn new(_0 : A, _1 : A) -> Vect<A> {
+        Vect(_0, _1)
     }
 
-    pub fn ortho_left(&self) -> Vect<A> {
+    /// determinant of two vectors a b : (a0 * b1 - a1 * b0) 
+    pub fn det(&self, other : &Vect<A>) -> A {
+        self.0 * other.1 - self.1 * other.0
+    }
+
+    /// dot product of two vectors
+    pub fn dot(&self, other : &Vect<A>) -> A {
+        self.0 * other.0 + self.1 * other.1
+    }
+
+    /// checks linear dependency of two vectors by checking if determinant is in range [0 - eps, 0 + eps]
+    pub fn lin_dep(&self, other : &Vect<A>, eps : A) -> bool {
+        self.det(other).inc_in(A::zero() - eps, A::zero() + eps)
+    }
+
+    /// returns determinant if two vectors are linear independent
+    pub fn lin_dep_det(&self, other : &Vect<A>, eps : A) -> Option<A> {
+        let det = self.det(other); 
+
+        match det.inc_in(A::zero() - eps, A::zero() + eps) {
+            false => None,
+            true  => Some(det)
+        }
+    }
+
+    /// rotates vector 90°
+    pub fn orth_l(&self) -> Vect<A> {
         Vect::new(-self.1, self.0)
+    }
+
+    /// rotates vector 270°
+    pub fn orth_r(&self) -> Vect<A> {
+        Vect::new(self.1, -self.0)
     }
 }
 
@@ -77,20 +123,91 @@ impl<A : Num> PTri<A> {
     }
 }
 
-impl<A : Num> DPRect<A> {
-    pub fn new(a : Vect<A>, b : Vect<A>, dist : A) -> DPRect<A> {
-        DPRect{pnts : (a, b), dist : dist}
-    }
-
-    pub fn contour(&self) -> [Vect<A>; 4] {
-        let (a, b) = self.pnts;
-        let ortho_ab = (b - a).ortho_left();
-        let c = a + ortho_ab;
-        let d = b + ortho_ab;
-
-        [a, b, c, d]
+impl<A : Num> PDRect<A> {
+    pub fn new(a : Vect<A>, b : Vect<A>, dist_ratio : A) -> PDRect<A> {
+        PDRect{pnts : (a, b), disr : dist_ratio}
     }
 }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+//   TRAITS 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+pub trait Line<A : Num> {
+    /// points that define line
+    fn pnts(&self) -> (Vect<A>, Vect<A>);
+
+    /// position vector of line
+    fn pos(&self) -> Vect<A> {
+        self.pnts().0
+    }
+
+    /// direction vector of line
+    fn dir(&self) -> Vect<A> {
+        self.pnts().1
+    }
+
+    /// optional intersection between to lines : a + r * b
+    ///
+    /// a + r * b = c + s * d
+    ///
+    /// => r = (det(d, c) + det(a, d)) / det(d, b)
+    fn intsec<B : Line<A>>(&self, other : &B, eps : A) -> Option<Vect<A>> {
+        let a = self.pos();
+        let b = self.dir();
+        let c = other.pos();
+        let d = other.dir();
+
+        let div = Vect::lin_dep_det(&d, &b, eps)?;
+        let r = (Vect::det(&d, &c) + Vect::det(&a, &d)) / div;
+
+        Some(a + b * r)
+    }
+}
+
+pub trait Triangle<A : Num> {
+    fn pnts(&self) -> (Vect<A>, Vect<A>, Vect<A>);
+}
+
+pub trait Rectangle<A : Num> {
+    /// a side of the rectangle
+    /// 
+    /// a = side.0, b = side.1
+    /// 
+    /// c = b + disr * rotate(90, b - a)
+    /// 
+    /// d = a + disr * rorate(90, b - a)
+    fn side(&self) -> (Vect<A>, Vect<A>);
+
+    /// left orthogonal distance ratio to self.side()
+    /// 
+    /// a = side.0, b = side.1
+    /// 
+    /// c = b + disr * rotate(90, b - a)
+    /// 
+    /// d = a + disr * rorate(90, b - a)
+    fn disr(&self) -> A;
+
+    /// checks if rectangle contains point  
+    fn contains(&self, pnt : &Vect<A>) -> bool {
+        let (a, b) = self.side();
+        let disr = self.disr();
+
+        let ab = b - a;
+        let c  = b + ab.orth_l() * disr;
+        let ac = c - a;
+        let ap = *pnt - a;
+
+        let ap_dot_ab = ap.dot(&ab);
+        let ap_dot_ac = ap.dot(&ac);
+
+        A::zero() < ap_dot_ab && ap_dot_ab < ab.dot(&ab) && A::zero() < ap_dot_ac && ap_dot_ac < ac.dot(&ac)
+    }
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+//   TRAIT IMPLEMENTATIONS 
+// ----------------------------------------------------------------------------------------------------------------------------------------
 
 impl<A : Num> Clone for Vect<A> {
     fn clone(&self) -> Vect<A> {
@@ -116,13 +233,13 @@ impl<A : Num> Clone for PTri<A> {
     }
 }
 
-impl<A : Num + Copy> Copy for Vect<A> {}
+impl<A : Num> Copy for Vect<A> {}
 
-impl<A : Num + Copy> Copy for PLine<A> {}
+impl<A : Num> Copy for PLine<A> {}
 
-impl<A : Num + Copy> Copy for VLine<A> {}
+impl<A : Num> Copy for VLine<A> {}
 
-impl<A : Num + Copy> Copy for PTri<A> {}
+impl<A : Num> Copy for PTri<A> {}
 
 impl<A : Num> Add for Vect<A> {
     type Output = Vect<A>;
@@ -152,7 +269,7 @@ impl<A : Num> Mul<A> for Vect<A> {
     type Output = Vect<A>;
 
     fn mul(self, other : A) -> Vect<A> {
-        Vect::new(self.0 * other.clone(), self.1 * other)
+        Vect::new(self.0 * other, self.1 * other)
     }
 }
 
@@ -168,6 +285,14 @@ impl<A : Num> Div<A> for Vect<A> {
     type Output = Vect<A>;
 
     fn div(self, other : A) -> Vect<A> {
-        Vect::new(self.0 / other.clone(), self.1 / other)
+        Vect::new(self.0 / other, self.1 / other)
+    }
+}
+
+impl<A : Num> Neg for Vect<A> {
+    type Output = Vect<A>;
+
+    fn neg(self) -> Vect<A> {
+        Vect::new(-self.0, -self.1)
     }
 }
