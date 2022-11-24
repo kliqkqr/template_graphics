45459b1264de594cefcffbad5100e3c90fc3ae6a
kliqkqr <paul.prietzel@gmail.com>
2022-11-24 19:14:49 +0100

    tested rectangle.contains and drawing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use tg::file::stl::Stl;
+use image::*;
 
-use tg::prim::d3::Vect;
-
-fn vect_to_string(vect : Vect<f32>) -> String {
-    format!("({:.5}, {:.5}, {:.5})", vect.0, vect.1, vect.2)
-}
+use tg::geom::prim::d2::*;
+use tg::draw::rast::*;
 
 fn main() {
-    let stl_path = r#"C:\OneDrive\Code\Bachelor\models\stl\LINK SP-CL 3D\177-200_26.stl"#;
-    let stl = match Stl::read_binary(stl_path) {
-        Ok(stl) => stl,
-        Err(err) => {
-            println!("{}", err.to_string());
-            return;
-        }
-    };
+    let mut image = RgbImage::new(1000, 1000);
 
-    println!("head: {}", stl.head().iter().map(|byte| *byte as char).collect::<String>());
-    println!("size: {}", stl.triangles().len());
+    let a = Vect::new(450f32, 100f32);
+    let b = Vect::new(550f32, 100f32);
 
-    for (i, t) in stl.triangles().iter().enumerate() {
-        let normal= t.normal();
-        let vertices = t.vertices();
-        let attr= t.attr_byte_count();
+    let s = PSeg::new(a, b);
+    let r = 8f32;
+    let rect = SRect::new(s, r);
 
-        println!("");
-        println!("indx: {}", i);
-        println!("norm: {}", vect_to_string(normal));
-        println!("v[0]: {}", vect_to_string(vertices.0));
-        println!("v[1]: {}", vect_to_string(vertices.1));
-        println!("v[2]: {}", vect_to_string(vertices.2));
-        println!("attr: {}", attr);
+    let pnts = rect.points();
+
+    println!("dir: {}", rect.seg.dir().to_string());
+
+    for p in pnts {
+        println!("{}", p.to_string())
     }
+
+    let color = Rgb::from([255u8, 0u8, 0u8]);
+
+    fill_rect(&mut image, &rect, color);
+
+    let _ = image.save("test.bmp");
 }
