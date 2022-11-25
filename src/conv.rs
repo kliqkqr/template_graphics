1c1760932e93b7324a1147a15b2765397a45cf87
kliqkqr <paul.prietzel@gmail.com>
2022-11-25 20:57:02 +0100

    added From and To Trait

--- src/conv.rs
+++ src/conv.rs
@@ -4,3 +4,24 @@ pub trait Easy<A> {
     fn easy(self) -> A;
 }
 
+/// trait for conversion from A to Self
+pub trait From<A> {
+    fn from(_ : A) -> Self;
+}
+
+/// trait for conversion from Self to A - inverse of From trait if implemented
+pub trait To<A> {
+    fn to(self) -> A;
+}
+
+impl<A : std::convert::From<B>, B> From<B> for A {
+    fn from(b : B) -> Self {
+        <A as std::convert::From<B>>::from(b)
+    }
+}
+
+impl<A, B> To<B> for A where B : From<A> {
+    fn to(self) -> B {
+        B::from(self)
+    }
+}
