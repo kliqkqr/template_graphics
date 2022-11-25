eac72492c5559a97b6b9c87f00b7415fe32000c9
kliqkqr <paul.prietzel@gmail.com>
2022-11-25 03:42:31 +0100

    fill_rect refactor / draw_line added

--- src/draw/rast.rs
+++ src/draw/rast.rs
@@ -6,6 +6,9 @@ use image::{
 use crate::geom::d2::prim::{
     Vect,
     TRect,
+    SRect,
+    BRect,
+    Segment,
     Rectangle
 };
 
@@ -15,20 +18,36 @@ use crate::range::{
 
 pub fn fill_rect<A : Rectangle<f32> + std::fmt::Debug>(image : &mut RgbImage, rect : &A, color : Rgb<u8>) {
     let (width, height) = image.dimensions();
-    let dims = Vect::new(width, height);
+    let dims = BRect::start_zero_unchecked(Vect::new(width, height));
 
-    let min = rect.min().map(|f| f.floor().max(0f32) as u32).max(&Vect::zero());
-    let max = rect.max().map(|f| f.ceil().max(0f32) as u32).max(&dims);
+    let bounds = rect.bounds().map(|v| v.map(|f| *f as u32));
+    let bounds = dims.clamp(bounds);
+
+    let start = bounds.start();
+    let end   = bounds.end();
 
     let rect = TRect::from(rect);
 
-    for x in range(min.0, max.0) {
-        for y in range(min.1, max.1) {
-            let pnt = Vect::new(x as f32, y as f32) + 0.5f32;
+    for x in range(start.0, end.0) {
+        for y in range(start.1, end.1) {
+            let point = Vect::new(x as f32, y as f32) + 0.5f32;
 
-            if rect.contains(&pnt) {
+            if rect.contains(&point) {
                 image.put_pixel(x, y, color);
             }
         }
     }
+}
+
+pub fn draw_seg<A : Segment<f32>>(image : &mut RgbImage, seg : &A, color : Rgb<u8>, width : f32) {
+    let seg_dir = seg.dir();
+    let seg_len = seg_dir.len();
+    let offset_ratio = (width / 2.0) / seg_len;
+    let seg_offset = seg_dir.orth_r() * offset_ratio;
+
+    let offset_seg = seg.add(&seg_offset);
+    let rect_ratio = width / seg_len;
+    let rect = SRect::new(offset_seg, rect_ratio);
+
+    fill_rect(image, &rect, color);
 }
