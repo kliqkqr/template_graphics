77f90ce15ea9b889bd9b0560d51f0e133e298f85
kliqkqr <paul.prietzel@gmail.com>
2022-12-08 00:04:31 +0100

    removed some tests

--- src/draw/rast.rs
+++ src/draw/rast.rs
@@ -55,35 +55,3 @@ pub fn draw_seg<A : Segment<f32>>(image : &mut RgbImage, seg : &A, color : Rgb<u
 
     fill_rect(image, &rect, color);
 }
-
-pub fn fill_tri_bary<T>(image : &mut RgbImage, tri : &T, color : Rgb<u8>)
-where T : crate::geom::d2::prim::refactor::tri::Triangle<Val = f32>
-{
-    let (width, height) = image.dimensions();
-
-    for x in 0..width {
-        for y in 0..height {
-            let point = (x as f32, y as f32).vadd(0.5f32);
-
-            if tri.contains_bary(&point) {
-                image.put_pixel(x, y, color);
-            }
-        }
-    }
-}
-
-pub fn fill_tri_norm<T>(image : &mut RgbImage, tri : &T, color : Rgb<u8>)
-where T : crate::geom::d2::prim::refactor::tri::Triangle<Val = f32>
-{
-    let (width, height) = image.dimensions();
-
-    for x in 0..width {
-        for y in 0..height {
-            let point = (x as f32, y as f32).vadd(0.5f32);
-
-            if tri.contains_norm(&point) {
-                image.put_pixel(x, y, color);
-            }
-        }
-    }
-}
